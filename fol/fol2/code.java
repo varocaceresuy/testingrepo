fd3ddc1e491472a642137a1c6da8c69fd953fb15
Marcos Boggia <marcos_boggia@hotmail.com>
2021-12-23 21:06:18 -0300

    Update code.java

--- fol/fol2/code.java
+++ fol/fol2/code.java
@@ -2,7 +2,3 @@ class Code {
     public static void main(String[] args) {
         System.out.println("dsaddsadsaaWorld");
     }}
-
-𝐍ø Code.mainø❶❼_❶❶❶
-𝐓ø❶❼ø❶❶❶þþ java, string, printing
-𝐅ø❶❼ø❶❶❶þþ The main method
