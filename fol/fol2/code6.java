3eb7325db4ee853388139fe2c838fe15f8b84566
Marcos Boggia <marcos_boggia@hotmail.com>
2022-02-08 15:18:36 -0300

    Update code6.java

--- fol/fol2/code6.java
+++ fol/fol2/code6.java
@@ -1,5 +1,1 @@
-class Code { public static void main(String[] args) { System.out.println("Hdddasdsadassadaello dsadasdsadasWorld"); } }
-
-𝐍ø Code.mainø❶❹_❶❶❽
-𝐓ø❶❹ø❶❶❽þþ java
-𝐅ø❶❹ø❶❶❽þþ This is the program entry point
+class Animals { public static void main(String[] args) { String dog = "dog"; } }
