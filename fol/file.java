4d7a9b1ba934f68f2153ce350f94a9ea635cf910
Marcos Boggia <marcos_boggia@hotmail.com>
2022-02-08 15:16:57 -0300

    Update file.java

--- fol/file.java
+++ fol/file.java
@@ -1,1 +1,1 @@
-class Code { public static void main(String[] args) { System.out.println("Heasdaslasdalo World"); } }
+class Code { public static void main(String[] args) { System.out.println("Hello World"); } }
