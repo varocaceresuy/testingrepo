c5b63b172c971de0243fae8b1f33c3a05d709333
Marcos Boggia <marcos_boggia@hotmail.com>
2022-03-15 12:33:29 -0300

    Update asd.java

--- fol/asd.java
+++ fol/asd.java
@@ -1,32 +1,6 @@
 public class Message {
   int x = 5;
-
   public static void message(String[] args) {
-    Message myObj = new Message();
-    System.out.println(myObj.x);
-  }
-  /**
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string} message
-  * @param {string} title
-  * @param {string}
-  */
-  public static void function(){
     Message myObj = new Message();
     System.out.println(myObj.x);
   }
