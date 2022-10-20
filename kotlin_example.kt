00a522ee618a9d68a8d98eec39c6f3263e7a0307
Marcos Boggia <marcos_boggia@hotmail.com>
2022-10-19 23:46:45 -0300

    Create kotlin_example.kt

---
+++ kotlin_example.kt
@@ -0,0 +1,18 @@
+package com.example.test
+
+import android.app.Activity
+import android.app.Application
+
+class ExampleClass : ParentClass {
+	/**
+	* Doc description
+	*
+	* @return return description.
+	*/
+    fun sum(a: int, b:int) {
+        return a + b
+    }
+
+    fun empty_function()
+}
+
